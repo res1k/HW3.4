4ca69239fa24e4916397af808e7e3020c961f82a
Andrey Zhivotov <res_a@mail.ru>
2022-07-02 12:09:26 +0300

    Initial Commit

---
+++ HW3.4/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HW3.4
+//
+//  Created by Andrey Zhivotov on 02.07.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HW3.4/HW3_4App.swift
@@ -0,0 +1,17 @@
+//
+//  HW3_4App.swift
+//  HW3.4
+//
+//  Created by Andrey Zhivotov on 02.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct HW3_4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
